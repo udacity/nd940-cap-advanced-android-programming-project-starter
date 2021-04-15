75ac9dd9648e30ec6c20760a66b8e5ad529e616d
Vladimir Espinola Lezcano <vladimir.espinola@gmail.com>
2021-04-15 18:28:27 -0400

    added attribute for representatives

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -1,10 +1,16 @@
 package com.example.android.politicalpreparedness.representative
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.politicalpreparedness.data.network.models.Address
 
 class RepresentativeViewModel: ViewModel() {
 
     //TODO: Establish live data for representatives and address
+    private val _address = MutableLiveData<Address>()
+    val address: LiveData<Address>
+        get() = _address
 
     //TODO: Create function to fetch representatives from API from a provided address
 
